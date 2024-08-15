51cff24fbd43b13a336288dee7c2713fd5c79491
Arussel <anhminh05102004@gmail.com>
2024-08-14 23:08:40 -0400

    Fix Typescript bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useNavigate, Routes, Route } from 'react-router-dom';
 import Logo from './assets/images/logoNoBackground.png';
 import CartIcon from './assets/images/cart.svg';
 import CircleIcon from './assets/images/circle.svg';
@@ -13,9 +12,11 @@ import CartComponent from './components/cart/Cart';
 import './App.css';
 
 interface Item {
-  id: string;
-  name: string;
-  counter: number;
+  id: string; 
+  title: string; 
+  price: number; 
+  counter: number; 
+  image: string
 }
 
 const App = () => {
@@ -74,17 +75,17 @@ const App = () => {
           </div>
         </button>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/shop/detail" element={<Detail addItems={addItems} />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/cart"
-          element={<CartComponent itemsList={items} setItemsList={setItems} />}
-        />
-        <Route path="*" element={<Error />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop/detail" element={<Detail addItems={addItems} />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/cart"
+            element={<CartComponent itemsList={items} setItemsList={setItems} />}
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
     </div>
   );
 };
